3f9fe78330507a20f8a56b9e1339a18d97d3e5fc
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-22 08:47:45 +0000

    only index page is working

--- db/migrate/20171121212215_create_posts.rb
+++ db/migrate/20171121212215_create_posts.rb
@@ -1,11 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :index
-      t.string :show
-      t.string :edit
-      t.string :new
-      t.string :destroy
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
